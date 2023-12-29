940236cd1389083e32b4b233c32e0f182f2fce2b
Surya <suryababu.sakthivel@uber.com>
2023-12-30 01:38:24 +0530

    initial setup

--- src/app/about-us/page.tsx
+++ src/app/about-us/page.tsx
@@ -1,9 +1,9 @@
 import Image from "next/image";
 
-export default function Home() {
+export default function AboutUs() {
   return (
     <div>
-      <h1>Home</h1>
+      <h1>About Us</h1>
     </div>
   );
 }
