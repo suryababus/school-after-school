6526f897b720cca88d1c0a0371e002894685ca7a
Surya <suryababu.sakthivel@uber.com>
2023-12-30 11:10:27 +0530

    setup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,7 +17,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <div className="tool-bar">tab bar</div>
+        {children}
+      </body>
     </html>
   );
 }
