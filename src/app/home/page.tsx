e9cf699cf89cdd4bde863354759dfa40194b4117
Surya <suryababu.sakthivel@uber.com>
2024-01-02 22:09:32 +0530

    nav bar

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,6 +1,12 @@
 import { MainCarousel } from "@/components/home/main-carousel";
+import { NavBar } from "@/components/home/nav-bar";
 import Image from "next/image";
 
 export default function Home() {
-  return <MainCarousel />;
+  return (
+    <>
+      <NavBar />
+      <MainCarousel />
+    </>
+  );
 }
---
+++ src/components/home/nav-bar.tsx
@@ -0,0 +1,60 @@
+"use client";
+import React from "react";
+
+// add images as the carousel items background
+// images are from public/
+// use next/image to optimize images
+
+const NavBarItems = [
+  {
+    name: "Home",
+    link: "/",
+  },
+  {
+    name: "Our Work",
+    link: "/work",
+  },
+  {
+    name: "News Letter",
+    link: "/news",
+  },
+  {
+    name: "About Us",
+    link: "/about",
+  },
+  {
+    name: "Contact Us",
+    link: "/contact",
+  },
+];
+
+export const NavBar = () => {
+  const [route, setRoute] = React.useState("/");
+
+  const handleRoute = (route: string) => {
+    setRoute(route);
+  };
+  return (
+    <div className="relative h-0 flex w-full justify-center items-center">
+      <div className="bg-white flex flex-row mt-32 z-10 rounded-xl overflow-hidden">
+        {NavBarItems.map((item, index) => {
+          return (
+            <div
+              className={`p-2 border-orange-500 ${
+                route === item.link ? "border-b-4" : ""
+              }`}
+            >
+              <div
+                key={index}
+                onClick={() => handleRoute(item.link)}
+                className="px-4 py-2 hover:bg-gray-100 cursor-pointer "
+              >
+                {item.name}
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
